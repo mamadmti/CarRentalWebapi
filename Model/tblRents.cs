eefd36db12ee2939f2a792ddaea0ca36e3ad2bf5
Mohammad Mokhtari <mti1393@gmail.com>
2021-11-03 15:58:11 +0330

    Database Structure Created

---
+++ Model/tblRents.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRentalProject.Model
+{
+    public class tblRents
+    {
+        public int Id { get; set; }
+
+        // ارتباط با جدول یوزرز
+        public tblUsers TblUsers { get; set; }
+        public int TblUsersId { get; set; }
+
+        //ارتباط با جدول ماشین ها
+        public tblCars TblCars { get; set; }
+        public int TblCarsId { get; set; }
+
+
+    }
+}
