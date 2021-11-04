812d14c48da12cd3fdeb92e0a537a3d9b366b366
Mohammad Mokhtari <mti1393@gmail.com>
2021-11-04 13:35:45 +0330

    app setting modified & Dto Created

---
+++ Dto/DtoBrands.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRentalProject.Dto
+{
+    public class DtoBrands
+    {
+        public class Post
+        {
+           [Required]
+           public string BrandName { get; set; }
+        }
+
+        public class Put:Post
+        {
+            
+        }
+
+
+    }
+}
---
+++ Dto/DtoGearboxType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRentalProject.Dto
+{
+    public class DtoGearboxType
+    {
+        public class Post
+        {
+            [Required]
+            public string GearBoxTypeName { get; set; }
+        }
+
+        public class Put : DtoGearboxType.Post
+        {
+
+        }
+    }
+}
